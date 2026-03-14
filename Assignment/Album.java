82090b843678f5ac7a254ecc52bea77f16ee5141
ArafatSany7 <human.sany7@gmail.com>
2026-03-15 03:05:11 +0600

    Person

---
+++ Assignment/Album.java
@@ -0,0 +1,32 @@
+import java.time.Year;
+
+public class Album {
+    private String title;
+    private String genre;
+    private int releaseYear;
+
+    public Album(String title, String genre, int releaseYear) {
+        this.title = title;
+        this.genre = genre;
+        this.releaseYear = releaseYear;
+    }
+
+    public Album(String title, String genre) {
+        this(title, genre, Year.now().getValue());
+    }
+
+    public void display() {
+        System.out.println("Title: " + title + ", Genre: " + genre + ", Release Year: " + releaseYear);
+    }
+
+    public static void main(String[] args) {
+
+        Album album1 = new Album("Abbey Road", "Rock", 1969);
+
+        Album album2 = new Album("Green River", "Rock");
+
+        album1.display();
+        album2.display();
+
+    }
+}
---
+++ Assignment/Person.java
@@ -0,0 +1,35 @@
+public class Person {
+    private String name;
+    private int age;
+
+    public Person() {
+        this.name = "Unknown";
+        this.age = 0;
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person(Person other) {
+        this.name = other.name;
+        this.age = other.age;
+    }
+
+    public void display() {
+        System.out.println("Name: " + name + ", Age: " + age);
+    }
+
+    public static void main(String[] args) {
+        Person unknownPerson = new Person();
+        unknownPerson.display();
+
+        Person alice = new Person("Alice", 30);
+        alice.display();
+
+        Person aliceCopy = new Person(alice);
+        aliceCopy.display();
+
+    }
+}

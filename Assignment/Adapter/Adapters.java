5f9eb7d4d7f4fd1659a519c53d28d1d95c7ad2c7
ArafatSany7 <human.sany7@gmail.com>
2026-04-04 22:24:24 +0600

    Adapter

---
+++ Assignment/Adapter/Adapters.java
@@ -0,0 +1,30 @@
+interface Hdmi {
+    void connectWithHdmiCable();
+}
+
+class Dvi {
+    public void connectWithDviCable() {
+        System.out.println("Connected with DVI cable.");
+    }
+}
+
+class Adapter implements Hdmi {
+    private Dvi dvi;
+
+    public Adapter(Dvi dvi) {
+        this.dvi = dvi;
+    }
+
+    @Override
+    public void connectWithHdmiCable() {
+        dvi.connectWithDviCable();
+    }
+}
+
+public class Adapters {
+    public static void main(String[] args) {
+        Dvi dvi = new Dvi();
+        Hdmi adapter = new Adapter(dvi);
+        adapter.connectWithHdmiCable();
+    }
+}

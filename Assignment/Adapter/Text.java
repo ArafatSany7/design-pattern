279b6ac5312aaffe792a4fd278ac11250f8bfbd3
ArafatSany7 <human.sany7@gmail.com>
2026-04-05 20:51:57 +0600

    Text

---
+++ Assignment/Adapter/Text.java
@@ -0,0 +1,59 @@
+interface FormattedText {
+    String format();
+}
+
+class SimpleText implements FormattedText {
+    private final String text;
+
+    public SimpleText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public String format() {
+        return this.text;
+    }
+}
+
+abstract class TextDecorator implements FormattedText {
+    protected FormattedText decoratedText;
+
+    public TextDecorator(FormattedText text) {
+        this.decoratedText = text;
+    }
+
+    @Override
+    public String format() {
+        return decoratedText.format();
+    }
+}
+
+class BoldText extends TextDecorator {
+    public BoldText(FormattedText text) {
+        super(text);
+    }
+
+    @Override
+    public String format() {
+        return "<b>" + super.format() + "</b>";
+    }
+}
+
+class ItalicText extends TextDecorator {
+    public ItalicText(FormattedText text) {
+        super(text);
+    }
+
+    @Override
+    public String format() {
+        return "<i>" + super.format() + "</i>";
+    }
+}
+
+public class Text {
+    public static void main(String[] args) {
+        FormattedText text = new ItalicText(new BoldText(new SimpleText("Hello, World!")));
+
+        System.out.println(text.format());
+    }
+}

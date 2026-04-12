9a5248f4dcf0c1bbf7c7aa63d2cfaf8c6f7ee1a3
ArafatSany7 <human.sany7@gmail.com>
2026-04-12 17:07:10 +0600

    House

---
+++ Assignment/Facade/Home.java
@@ -0,0 +1,76 @@
+class SoundSystem {
+    public void start() {
+        System.out.println("Sound system starting...");
+    }
+
+    public void playSound() {
+        System.out.println("Sound system playing audio...");
+    }
+
+    public void stop() {
+        System.out.println("Sound system stopped.");
+    }
+}
+
+class Projector {
+    public void turnOn() {
+        System.out.println("Projector turning on...");
+    }
+
+    public void display() {
+        System.out.println("Projector displaying video...");
+    }
+
+    public void turnOff() {
+        System.out.println("Projector turning off.");
+    }
+}
+
+class Lights {
+    public void dim() {
+        System.out.println("Lights dimming...");
+    }
+
+    public void brighten() {
+        System.out.println("Lights brightening...");
+    }
+}
+
+class HomeTheaterFacade {
+    private Projector projector;
+    private SoundSystem soundSystem;
+    private Lights lights;
+
+    public HomeTheaterFacade() {
+        this.projector = new Projector();
+        this.soundSystem = new SoundSystem();
+        this.lights = new Lights();
+    }
+
+    public void watchMovie() {
+        System.out.println("Get ready to watch a movie...");
+        lights.dim();
+        projector.turnOn();
+        projector.display();
+        soundSystem.start();
+        soundSystem.playSound();
+    }
+
+    public void endMovie() {
+        System.out.println("Shutting movie theater down...");
+        soundSystem.stop();
+        projector.turnOff();
+        lights.brighten();
+    }
+}
+
+
+public class Home {
+    public static void main(String[] args) {
+         HomeTheaterFacade homeTheater = new HomeTheaterFacade();
+
+        homeTheater.watchMovie();
+        System.out.println("-------------------------");
+        homeTheater.endMovie();
+    }
+}

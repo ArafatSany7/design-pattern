20a351f2649d4ef15e72235d363199740d133880
ArafatSany7 <human.sany7@gmail.com>
2026-04-21 00:18:24 +0600

    facade2

---
+++ practice/facade/Ecommerce.java
@@ -0,0 +1,45 @@
+interface orderTask{
+    void execute(String productId , double amount);
+}
+
+class CheckInventory implements orderTask{
+    public void execute(String productId , double amount){
+        System.out.println("Product " + productId + "Is available and price is " + amount);
+    }
+}
+
+class CalculateAmount implements orderTask{
+    public void execute(String productId , double amount){
+        System.out.println("Total Amount is "+amount+" Taka");
+    }
+}
+class PaymentConfirmation implements orderTask{
+    public void execute(String productId ,  double amount ){
+        System.out.println("Payment tk " + amount + " Has confirmed for product id "+ productId);
+    }
+}
+
+class orderFacade {
+    private orderTask inventory;
+    private orderTask calculate;
+    private orderTask payment;
+
+    public orderFacade(){
+        this.calculate =  new CalculateAmount();
+        this.inventory  =  new CheckInventory();
+        this.payment =  new PaymentConfirmation();
+    }
+
+    public void placeOrder(String productID ,  double amount){
+        inventory.execute(productID, amount);
+        payment.execute(productID, amount);
+        calculate.execute(productID, amount);
+    }
+}
+public class Ecommerce {
+    public static void main(String[] args) {
+        orderFacade facade = new orderFacade();
+
+        facade.placeOrder("357678", 876);
+    }
+}
